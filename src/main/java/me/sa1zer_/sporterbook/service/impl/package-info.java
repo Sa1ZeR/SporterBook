4a798c61da9b80913dfbcadd67912bbaaedd48da
bifurcated <litvinenko-maks@inbox.ru>
2022-05-23 20:04:47 +1000

    Add doc for service package

---
+++ src/main/java/me/sa1zer_/sporterbook/service/impl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Implementation of database services.
+ */
+package me.sa1zer_.sporterbook.service.impl;
---
+++ src/main/java/me/sa1zer_/sporterbook/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Services for working with databases.
+ */
+package me.sa1zer_.sporterbook.service;
