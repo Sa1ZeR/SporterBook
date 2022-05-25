cf9b2c7bb2fc3010297e34d044875e18332ee609
bifurcated <litvinenko-maks@inbox.ru>
2022-05-25 22:18:54 +1000

    Add javadoc to additional packages

---
+++ src/main/java/me/sa1zer_/sporterbook/api/admin/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * API used for the Admin to interact with the services.
+ */
+package me.sa1zer_.sporterbook.api.admin;
---
+++ src/main/java/me/sa1zer_/sporterbook/payload/handler/handle/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Basic request handling.
+ */
+package me.sa1zer_.sporterbook.payload.handler.handle;
