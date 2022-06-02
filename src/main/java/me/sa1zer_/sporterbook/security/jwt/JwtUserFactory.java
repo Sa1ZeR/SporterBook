55beb82e9ca589efbe45834c8642f08118423560
Serg1K <amsher2007@mail.ru>
2022-06-02 12:42:35 +1000

    return all permissions

--- src/main/java/me/sa1zer_/sporterbook/security/jwt/JwtUserFactory.java
+++ src/main/java/me/sa1zer_/sporterbook/security/jwt/JwtUserFactory.java
@@ -1,9 +1,11 @@
 package me.sa1zer_.sporterbook.security.jwt;
 
 import me.sa1zer_.sporterbook.domain.model.User;
+import me.sa1zer_.sporterbook.domain.model.enums.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +20,9 @@ public final class JwtUserFactory {
     }
 
     private static List<GrantedAuthority> getAuthorities(User user) {
+        if(user.getRoles().contains(Role.ADMIN))
+            return Arrays.stream(Role.values()).map(role -> new SimpleGrantedAuthority(role.name()))
+                    .collect(Collectors.toList());
         return user.getRoles().stream().map(r ->
                 new SimpleGrantedAuthority(r.name())).collect(Collectors.toList());
     }
